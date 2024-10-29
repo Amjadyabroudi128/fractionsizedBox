669c33908ad23dc051b18c32faeafecc22f37413
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-10-29 12:25:42 +0000

    widget

--- lib/main.dart
+++ lib/main.dart
@@ -35,14 +35,28 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Container(
         color: myContainer,
         child: Center(
-          child: FractionallySizedBox(
-            widthFactor: 0.9, // 90 percent of the parent widget
-            heightFactor: 0.4, // 40 percent of the parent widget
-            child: Container(
-              color: smalContainer,
-            ),
-          ),
+          child: fractionSB(smalContainer: smalContainer),
         )
+      ),
+    );
+  }
+}
+
+class fractionSB extends StatelessWidget {
+  const fractionSB({
+    super.key,
+    required this.smalContainer,
+  });
+
+  final Color smalContainer;
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 0.9, // 90 percent of the parent widget
+      heightFactor: 0.4, // 40 percent of the parent widget
+      child: Container(
+        color: smalContainer,
       ),
     );
   }
